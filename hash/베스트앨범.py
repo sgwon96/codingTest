24be5496700f7cfaa569e310c0ba378d990bf703
jmjung <jaemin032@naver.com>
2021-08-27 01:39:39 +0900

    1일차. 베스트 앨범 코드

--- hash/베스트앨범.py
+++ hash/베스트앨범.py
@@ -1,12 +1,21 @@
 from collections import defaultdict
 
 
-def solution(clothes):
-    closet = defaultdict(lambda: [])
-    for name, item_type in clothes:
-        closet[item_type].append(name)
+def solution(genres, plays):
+    genre_songs = defaultdict(list)
+    genre_play_count = defaultdict(int)
 
-    answer = 1
-    for items in closet.values():
-        answer *= len(items) + 1
-    return answer - 1
+    for i in range(len(genres)):
+        genre_songs[genres[i]].append([i, plays[i]])
+        genre_play_count[genres[i]] += plays[i]
+
+    genre_play_count = [(genre, plays) for genre, plays in genre_play_count.items()]
+    genre_play_count.sort(key=lambda x: x[1], reverse=True)
+
+    answer = []
+    for genre, play_count in genre_play_count:
+        genre_songs[genre].sort(key=lambda x: (x[1], -x[0]), reverse=True)
+        for song_num, play_count in genre_songs[genre][:2]:
+            answer.append(song_num)
+
+    return answer
